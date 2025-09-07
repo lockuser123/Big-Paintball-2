4dd8f98360951b14f924d48a05a7f5cea5e5ea89
lockuser123 <158370620+lockuser123@users.noreply.github.com>
2025-09-07 13:05:49 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -23,5 +23,15 @@ local Window = WindUI:CreateWindow({
             print("clicked")
         end,
     },
-    
-  
+
+        local Tab = Window:Tab({
+    Title = "Auto Farm",
+    Icon = "target",
+    Locked = false,
+})
+
+        local Tab = Window:Tab({
+    Title = "`Target",
+    Icon = "bird",
+    Locked = false,
+})
